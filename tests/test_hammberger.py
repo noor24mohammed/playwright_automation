5665a32b3204f03cf9e2af1863e31f99048d4743
noor24mohammed <136052654+noor24mohammed@users.noreply.github.com>
2026-01-09 19:10:33 +0530

    clone code after reimage

--- tests/test_hammberger.py
+++ tests/test_hammberger.py
@@ -7,7 +7,7 @@ def load_login_data():
     file_path = os.path.join(
         os.path.dirname(__file__),
         "data",
-        "DTE_data.json"
+        "dte_app_login_data.json"
     )
 
     with open(file_path, "r", encoding="utf-8") as f:
